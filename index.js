69e5944f05b0e635c248b92532703a5ace053fb6
Rishabh <rishabhverma.1202@gmail.com>
2024-03-11 10:33:04 +0530

    Added require files

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+/* configure body-parser */
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const { auth_route, user_route, product_route, cart_route, order_route } = require('./routes');
+
+app.use('/api/v1/auth', auth_route);
+app.use('/api/v1/users', user_route);
+app.use('/api/v1/products', product_route);
+app.use('/api/v1/carts', cart_route);
+app.use('/api/v1/orders', order_route);
+
+const dbConfig = require('./config/database-config-example');
+
+/* connecting to the database */
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+/* listen for requests */
+const port = process.env.PORT || 4000
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
